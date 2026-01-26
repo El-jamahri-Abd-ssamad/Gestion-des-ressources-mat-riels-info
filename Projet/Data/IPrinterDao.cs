3d68dc1808896451c44f7ae30a79b04e28c6dbaa
Hajar Abidi <hajarabidi03@gmail.com>
2026-01-26 23:19:17 +0100

    ressources

---
+++ Projet/Data/IPrinterDao.cs
@@ -0,0 +1,15 @@
+﻿
+using Projet.Domain;
+
+namespace Projet.Data
+{
+    public interface IPrinterDao
+    {
+        void AddPrinter(Printer printer);
+        Printer GetPrinter(string inventoryNumber);
+        List<Printer> GetPrinters();
+        List<Printer> GetPrintersByDepartment(int departmentId);
+        void UpdatePrinter(string inventoryNumber, Printer newPrinter);
+        void DeletePrinter(string inventoryNumber);
+    }
+}
---
+++ Projet/Models/ComputerDto.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ComputerDto : ResourceDto
+    {
+        [Required(ErrorMessage = "Le CPU est obligatoire")]
+        [StringLength(100, ErrorMessage = "Le CPU ne peut pas dépasser 100 caractères")]
+        public string CPU { get; set; }
+
+        [Required(ErrorMessage = "La RAM est obligatoire")]
+        [StringLength(50, ErrorMessage = "La RAM ne peut pas dépasser 50 caractères")]
+        public string RAM { get; set; }
+
+        [Required(ErrorMessage = "Le disque dur est obligatoire")]
+        [StringLength(50, ErrorMessage = "Le disque dur ne peut pas dépasser 50 caractères")]
+        public string HardDrive { get; set; }
+
+        [Required(ErrorMessage = "L'écran est obligatoire")]
+        [StringLength(50, ErrorMessage = "L'écran ne peut pas dépasser 50 caractères")]
+        public string Screen { get; set; }
+
+        public ComputerDto()
+        {
+            CPU = string.Empty;
+            RAM = string.Empty;
+            HardDrive = string.Empty;
+            Screen = string.Empty;
+        }
+
+        public ComputerDto(string inventoryNumber, string brand, string cpu, string ram,
+                          string hardDrive, string screen, DateTime? deliveryDate,
+                          int? supplierId, string assignedTo, string assignmentType, int? departmentId)
+        {
+            InventoryNumber = inventoryNumber;
+            Brand = brand;
+            CPU = cpu;
+            RAM = ram;
+            HardDrive = hardDrive;
+            Screen = screen;
+            DeliveryDate = deliveryDate;
+            SupplierId = supplierId;
+            AssignedTo = assignedTo;
+            AssignmentType = assignmentType;
+            DepartmentId = departmentId;
+        }
+
+        public override string GetResourceType() => "Computer";
+
+        public override string ToString()
+        {
+            return $"Computer #{InventoryNumber} - {Brand} (CPU: {CPU}, RAM: {RAM}, HDD: {HardDrive}, Screen: {Screen})";
+        }
+    }
+}
